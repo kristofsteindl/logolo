15e70635df12b625b0fb0cb1538d3d692675ccc9
Kristóf Steindl <ksteindl@chemaxon.com>
2021-03-14 12:46:58 +0100

    added some validation into Controller

--- src/main/java/com/ksteindl/logolo/web/ProjectController.java
+++ src/main/java/com/ksteindl/logolo/web/ProjectController.java
@@ -5,10 +5,13 @@ import com.ksteindl.logolo.services.ProjectService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
 
 @RestController
 @RequestMapping("/api/project")
@@ -19,7 +22,10 @@ public class ProjectController {
 
     // TODO we should introduce inputObject as @ResquestBody instead of Entity (very bad practice) or even instead of DTO (still bad practice)
     @PostMapping("")
-    public ResponseEntity<Project> createNewProject(@RequestBody Project project) {
+    public ResponseEntity<?> createNewProject(@Valid @RequestBody Project project, BindingResult result) {
+        if (result.hasErrors()) {
+            return new ResponseEntity<String>("Invalid Object", HttpStatus.BAD_REQUEST);
+        }
         Project persisted = projectService.saveOrUpdateProject(project);
         return new ResponseEntity<Project>(persisted, HttpStatus.CREATED);
     }
