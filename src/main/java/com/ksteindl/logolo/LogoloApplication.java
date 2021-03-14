af2a2402c3b4eb04aaab5af8de2f4f4225991454
Kristóf Steindl <ksteindl@chemaxon.com>
2021-03-14 11:12:05 +0100

    init Spring project

---
+++ src/main/java/com/ksteindl/logolo/LogoloApplication.java
@@ -0,0 +1,13 @@
+package com.ksteindl.logolo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LogoloApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LogoloApplication.class, args);
+    }
+
+}
