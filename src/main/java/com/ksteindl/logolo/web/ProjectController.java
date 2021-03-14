691ec96a657f10dd9057843fc097a6ec41f61dc1
Kristóf Steindl <ksteindl@chemaxon.com>
2021-03-14 16:34:22 +0100

    created simpler errorMap for Project Controller

--- src/main/java/com/ksteindl/logolo/web/ProjectController.java
+++ src/main/java/com/ksteindl/logolo/web/ProjectController.java
@@ -6,12 +6,17 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/api/project")
@@ -24,7 +29,7 @@ public class ProjectController {
     @PostMapping("")
     public ResponseEntity<?> createNewProject(@Valid @RequestBody Project project, BindingResult result) {
         if (result.hasErrors()) {
-            return new ResponseEntity<String>("Invalid Object", HttpStatus.BAD_REQUEST);
+            return new ResponseEntity<>(result.getFieldErrors().stream().collect(Collectors.toMap(FieldError::getField, FieldError::getDefaultMessage)), HttpStatus.BAD_REQUEST);
         }
         Project persisted = projectService.saveOrUpdateProject(project);
         return new ResponseEntity<Project>(persisted, HttpStatus.CREATED);
