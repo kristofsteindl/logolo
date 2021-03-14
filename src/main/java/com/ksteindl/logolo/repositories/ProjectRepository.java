6d8ee4c431ce699207914a07d5e90d06949aedba
Kristóf Steindl <ksteindl@chemaxon.com>
2021-03-14 11:40:25 +0100

    filled Project, created ProjectRepository

---
+++ src/main/java/com/ksteindl/logolo/repositories/ProjectRepository.java
@@ -0,0 +1,11 @@
+package com.ksteindl.logolo.repositories;
+
+import com.ksteindl.logolo.domain.Project;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProjectRepository extends CrudRepository<Project, Long> {
+
+
+}
