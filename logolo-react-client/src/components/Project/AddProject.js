ce55acdd6b666a6e8e5a8ad3201c04ca53839958
Kristóf Steindl <ksteindl@chemaxon.com>
2021-03-20 14:12:52 +0100

    designed the input validation errors, fixed projectKey in Exception body

--- logolo-react-client/src/components/Project/AddProject.js
+++ logolo-react-client/src/components/Project/AddProject.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import PropTypes from "prop-types";
 import {connect} from "react-redux";
 import {createProject} from "../../actions/projectActions"
+import classnames from "classnames"
  
 class AddProject extends Component {
     constructor() {
@@ -58,32 +59,45 @@ class AddProject extends Component {
                                             name="projectName" 
                                             value={this.state.projectName}
                                             type="text" 
-                                            className="form-control form-control-lg " 
+                                            className={classnames("form-control form-control-lg", {
+                                                "is-invalid":errors.projectName
+                                            })}
                                             placeholder="Project Name" 
                                             onChange={this.onChange}
                                             />
-                                            <p>{errors.projectName}</p>
+                                            {errors.projectName && (
+                                                <div className="invalid-feedback">{errors.projectName}</div>
+                                            )}
+                                            
                                     </div>
                                     <div className="form-group">
                                         <input 
                                             name="projectKey" 
                                             value={this.state.projectKey}
                                             type="text" 
-                                            className="form-control form-control-lg" 
+                                            className={classnames("form-control form-control-lg", {
+                                                "is-invalid":errors.projectKey
+                                            })}
                                             placeholder="Unique Project Key"
                                             onChange={this.onChange}
                                             />
-                                            <p>{errors.projectKey}</p>
+                                            {errors.projectName && (
+                                                <div className="invalid-feedback">{errors.projectKey}</div>
+                                            )}
                                     </div>
                         
                                     <div className="form-group">
                                         <textarea 
                                             name="description" 
                                             value={this.state.description}
-                                            className="form-control form-control-lg" 
+                                            className={classnames("form-control form-control-lg", {
+                                                "is-invalid":errors.description
+                                            })}
                                             placeholder="Project Description"
                                             onChange={this.onChange}></textarea>
-                                            <p>{errors.description}</p>
+                                            {errors.projectName && (
+                                                <div className="invalid-feedback">{errors.description}</div>
+                                            )}
                                     </div>
                                     <h6>Start Date</h6>
                                     <div className="form-group">
