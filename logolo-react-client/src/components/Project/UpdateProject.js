c98814c92b1b55cdd8cd614b28939ade41aff087
Kristóf Steindl <ksteindl@chemaxon.com>
2021-03-21 11:31:05 +0100

    finished error displaying during update, still with some bugs

--- logolo-react-client/src/components/Project/UpdateProject.js
+++ logolo-react-client/src/components/Project/UpdateProject.js
@@ -13,13 +13,17 @@ class UpdateProject extends Component {
             projectKey: "",
             description: "",
             startDate: "",
-            endDate: ""
+            endDate: "",
+            errors: {}
         }
         this.onChange = this.onChange.bind(this);
         this.onSubmit = this.onSubmit.bind(this);
     }
     
     componentWillReceiveProps(nextProps){
+        if (nextProps.errors) {
+            this.setState({errors: nextProps.errors});
+        }
         const { id,
             projectName,
             projectKey,
@@ -35,6 +39,7 @@ class UpdateProject extends Component {
     }
 
     componentDidMount() {
+        console.log("componentDidMount")
         const {id} = this.props.match.params;
         this.props.getProject(id, this.props.history);
     }
@@ -55,6 +60,7 @@ class UpdateProject extends Component {
     }
 
     render() {
+        const {errors} = this.state
         return (
             <div className="project">
             <div className="container">
@@ -66,12 +72,15 @@ class UpdateProject extends Component {
                             <div className="form-group">
                                 <input 
                                     type="text" 
-                                    className="form-control form-control-lg " 
+                                    className={classnames("form-control form-control-lg ", {"is-invalid":errors.projectName})} 
                                     placeholder="Project Name" 
                                     name="projectName"
                                     value={this.state.projectName}
                                     onChange={this.onChange}
                                     />
+                                    {errors.projectName && (
+                                        <div className="invalid-feedback">{errors.projectName}</div>
+                                    )}
                             </div>
                             <div className="form-group">
                                 <input 
@@ -85,13 +94,16 @@ class UpdateProject extends Component {
                             </div>
                             <div className="form-group">
                                 <textarea 
-                                    className="form-control form-control-lg" 
+                                className={classnames("form-control form-control-lg ", {"is-invalid":errors.description})} 
                                     placeholder="Project Description"
                                     name="description"
                                     value={this.state.description}
                                     onChange={this.onChange}
                                     >
                                 </textarea>
+                                {errors.description && (
+                                    <div className="invalid-feedback">{errors.description}</div>
+                                )}
                             </div>
                             <h6>Start Date</h6>
                             <div className="form-group">
@@ -127,12 +139,15 @@ class UpdateProject extends Component {
 UpdateProject.propTypes = {
     getProject: PropTypes.func.isRequired,
     updateProject: PropTypes.func.isRequired,
-    project: PropTypes.object.isRequired
+    project: PropTypes.object.isRequired,
+    errors: PropTypes.object.isRequired
 
 };
 
+
 const mapStateToProps = state => ({
-    project:state.project.project
+    project:state.project.project,
+    errors: state.errors
 });
 
 export default connect(mapStateToProps, {getProject, updateProject})(UpdateProject);
