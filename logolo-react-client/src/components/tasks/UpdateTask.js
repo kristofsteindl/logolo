1b628e5a3403b0637c0bf045f1abe9d47d4a0937
Kristóf Steindl <ksteindl@chemaxon.com>
2021-04-10 15:23:16 +0200

    fixed 1st

--- logolo-react-client/src/components/tasks/UpdateTask.js
+++ logolo-react-client/src/components/tasks/UpdateTask.js
@@ -1,151 +1,173 @@
-import React, { Component } from 'react'
-import { Link } from 'react-router-dom';
+import React, { Component } from "react";
 import { connect } from "react-redux";
 import classnames from "classnames";
-import { getTask } from '../../actions/backlogActions';
-import { getProject } from '../../actions/projectActions';
+import { getTask } from "../../actions/backlogActions";
 import PropTypes from "prop-types";
 
 class UpdateTask extends Component {
-    constructor() {
-        super();
-        this.handleChange = this.handleChange.bind(this);
-        this.state = {
-            foo: "bar",
-            task: {
-                summary: "",
-                projectSequence: "",
-                priority: "",
-                status: "",
-                acceptanceCriteria: "",
-                dueDate: ""
-            },
-            project: {
-                projectName: ""
-            }
-        };
-        
-    }
+  constructor() {
+    super();
 
+    this.state = {
+      id: "",
+      projectSequence: "",
+      summary: "",
+      acceptanceCriteria: "",
+      status: "",
+      priority: "",
+      dueDate: "",
+      projectKey: ""
+    };
+    this.onChange = this.onChange.bind(this);
+    this.onSubmit = this.onSubmit.bind(this);
+  }
 
+  componentDidMount() {
+    const { projectKey, projectSequence } = this.props.match.params;
+    this.props.getTask(projectKey, projectSequence, this.props.history);
+  }
 
+  componentWillReceiveProps(nextProps) {
+    const {
+      id,
+      projectSequence,
+      summary,
+      acceptanceCriteria,
+      status,
+      priority,
+      dueDate,
+      projectKey
+    } = nextProps.task;
 
-    componentWillReceiveProps(nextProps) {
-        console.log("nextProps");
-        console.log(nextProps);
-        /*
-        this.setState({
-            task: nextProps.task,
-            project: nextProps.project
-        });
-        */
-    }
-    handleChange(event) {
-        console.log("hello");
-        console.log(event.target.name);
-        console.log(event.target.value);
-        this.setState({value: event.target.value});
-    }
+    this.setState({
+      id,
+      projectSequence,
+      summary,
+      acceptanceCriteria,
+      status,
+      priority,
+      dueDate,
+      projectKey
+    });
+  }
 
-    componentDidMount(){
-        const { projectKey, projectSequence } = this.props.match.params;
-        this.props.getProject(projectKey, this.props.history);
-        this.props.getTask(projectKey, projectSequence, this.props.history);
-    }
+  onChange(e) {
+    this.setState({ [e.target.name]: e.target.value });
+  }
 
+  onSubmit(e) {
+    e.preventDefault();
 
-    render() {
-        return (
-            <div className="add-PBI">
-                <div className="container">
-                    <div className="row">
-                        <div className="col-md-8 m-auto">
-                            <Link to="" className="btn btn-light">
-                                Back to Project Board
-                            </Link>
-                            <h4 className="display-4 text-center">Update Project Task</h4>
-                            <p className="lead text-center">Project Name: {this.state.project.projectName}, Project Key: {this.state.task.projectSequence}</p>
-                            <form>
-                            <div className="form-group">
-                            <input 
-                                type="text" 
-                                className="form-control form-control-lg" 
-                                name="summary" 
-                                placeholder="Project Task summary" 
-                                value={this.state.task.summary}
-                                onChange={(this.handleChange)}
-                            />
-                                
-                        </div>
-                        <div className="form-group">
-                            <textarea 
-                                className="form-control form-control-lg" 
-                                placeholder="Acceptance Criteria" 
-                                name="acceptanceCriteria"
-                                value={this.state.task.acceptanceCriteria}
-                                onChange={this.handleChange}
-                            />
-                        </div>
-                        <h6>Due Date</h6>
-                        <div className="form-group">
-                            <input 
-                                type="date" 
-                                className="form-control form-control-lg" 
-                                name="dueDate" 
-                                value={this.state.task.dueDate}
-                                onChange={this.handleChange}
-                            />
-                        </div>
-                                <div className="form-group">
-                                    <select 
-                                        className="form-control form-control-lg" 
-                                        name="priority"
-                                        value={this.state.task.priority}
-                                        onChange={this.handleChange}
-                                    >
-                                        
-                                        <option value={0}>Select Priority</option>
-                                        <option value={1}>High</option>
-                                        <option value={2}>Medium</option>
-                                        <option value={3}>Low</option>
-                                    </select>
-                                </div>
-        
-                                <div className="form-group">
-                                    <select 
-                                        className="form-control form-control-lg" 
-                                        name="status"
-                                        value={this.state.task.status}
-                                        onChange={this.handleChange}
-                                    >
-                                        <option value="">Select Status</option>
-                                        <option value="TO_DO">TO DO</option>
-                                        <option value="IN_PROGRESS">IN PROGRESS</option>
-                                        <option value="DONE">DONE</option>
-                                    </select>
-                                </div>
-        
-                                <input type="submit" className="btn btn-primary btn-block mt-4" />
-                            </form>
-                        </div>
-                    </div>
+    const UpdateProjectTask = {
+      id: this.state.id,
+      projectSequence: this.state.projectSequence,
+      summary: this.state.summary,
+      acceptanceCriteria: this.state.acceptanceCriteria,
+      status: this.state.status,
+      priority: this.state.priority,
+      dueDate: this.state.dueDate,
+      projectIdentifier: this.state.projectIdentifier,
+      create_At: this.state.create_At
+    };
+
+    console.log(UpdateTask);
+  }
+
+  render() {
+    return (
+      <div className="add-PBI">
+        <div className="container">
+          <div className="row">
+            <div className="col-md-8 m-auto">
+              <a href="#" className="btn btn-light">
+                Back to Project Board
+              </a>
+              <h4 className="display-4 text-center">Update Project Task</h4>
+              <p className="lead text-center">
+                Project Name: {this.state.projectKey} | Project Task ID:{" "}
+                {this.state.projectSequence}{" "}
+              </p>
+              <form onSubmit={this.onSubmit}>
+                <div className="form-group">
+                  <input
+                    type="text"
+                    className="form-control form-control-lg"
+                    name="summary"
+                    placeholder="Project Task summary"
+                    value={this.state.summary}
+                    onChange={this.onChange}
+                  />
                 </div>
+                <div className="form-group">
+                  <textarea
+                    className="form-control form-control-lg"
+                    placeholder="Acceptance Criteria"
+                    name="acceptanceCriteria"
+                    value={this.state.acceptanceCriteria}
+                    onChange={this.onChange}
+                  />
+                </div>
+                <h6>Due Date</h6>
+                <div className="form-group">
+                  <input
+                    type="date"
+                    className="form-control form-control-lg"
+                    name="dueDate"
+                    value={this.state.dueDate}
+                    onChange={this.onChange}
+                  />
+                </div>
+                <div className="form-group">
+                  <select
+                    className="form-control form-control-lg"
+                    name="priority"
+                    value={this.state.priority}
+                    onChange={this.onChange}
+                  >
+                    <option value={0}>Select Priority</option>
+                    <option value={1}>High</option>
+                    <option value={2}>Medium</option>
+                    <option value={3}>Low</option>
+                  </select>
+                </div>
+
+                <div className="form-group">
+                  <select
+                    className="form-control form-control-lg"
+                    name="status"
+                    value={this.state.status}
+                    onChange={this.onChange}
+                  >
+                    <option value="">Select Status</option>
+                    <option value="TO_DO">TO DO</option>
+                    <option value="IN_PROGRESS">IN PROGRESS</option>
+                    <option value="DONE">DONE</option>
+                  </select>
+                </div>
+
+                <input
+                  type="submit"
+                  className="btn btn-primary btn-block mt-4"
+                />
+              </form>
             </div>
-        )
-    }
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
-UpdateTask.prototypes = {
-    getTask: PropTypes.func.isRequired,
-    task: PropTypes.object.isRequired,
-    project: PropTypes.object.isRequired
-}
-
+UpdateTask.propTypes = {
+  getTask: PropTypes.func.isRequired,
+  task: PropTypes.object.isRequired
+};
 
 const mapStateToProps = state => ({
-    task: state.backlog.task,
-    project: state.project.project
-})
+  task: state.backlog.task
+});
 
-
-export default connect (mapStateToProps, {getTask, getProject })(UpdateTask);
+export default connect(
+  mapStateToProps,
+  { getTask }
+)(UpdateTask);
